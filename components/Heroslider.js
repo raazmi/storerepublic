0c8fe8a2db14ac2524b5ca1da31ddb26417dd6b9
MI Raaz <mail.miraaz@gmail.com>
2019-12-10 18:56:58 +0600

    View commit

--- components/Heroslider.js
+++ components/Heroslider.js
@@ -1,12 +1,36 @@
 import {Container} from 'reactstrap';
+import {ReactSVG} from 'react-svg';
 
 function Heroslider(){
     return(
         <div className="mi-heroslider d-flex align-items-center" style={{backgroundImage: 'url(/images/heroslider-bg.png)'}}>
             <Container>
                 <div className="mi-heroslider-inner text-center">
-                    <h1 className="display-1">Store Republic</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas, porro reprehenderit. Perferendis sunt, et fuga, sapiente illo ullam, dolores cum assumenda vel nulla tenetur. Consectetur libero iusto neque! Est, eos.</p>
+                    <h1 className="display-1">
+                        <span className="mi-color-theme">Store </span>
+                        <span class="mi-color-secondary">Republic</span>
+                    </h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid velit sunt, excepturi eum, qui quia dolor animi necessitatibus ex odit accusamus libero nam iure, aspernatur esse quis doloremque officiis tenetur.</p>
+                    <div className="mi-heroslider-icons">
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/code.svg"/>
+                        </div>
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/globe.svg"/>
+                        </div>
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/code.svg"/>
+                        </div>
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/code.svg"/>
+                        </div>
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/code.svg"/>
+                        </div>
+                        <div className="single-icon">
+                            <ReactSVG src="/images/feathericons/code.svg"/>
+                        </div>
+                    </div>
                 </div>
             </Container>
         </div>
